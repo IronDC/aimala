5a3f3d7c396c6b46f48e840d8a69ffd6313bd859
David Garcia <dgarciabravo13@gmail.com>
2020-03-30 19:24:11 +0200

    mofify back/app.js

--- back/app.js
+++ back/app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/back', {useNewUrlParser: true})
+  .connect(process.env.DBURL, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
