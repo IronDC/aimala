da4f03b1aaa1f69613d05fec4003a88145f89eb3
carballo <carballet@gmail.com>
2020-04-13 19:10:08 +0200

    Merge conflict

--- front/src/pages/OneGame.page.js
+++ front/src/pages/OneGame.page.js
@@ -5,18 +5,18 @@ import { Link } from "react-router-dom";
 const OneGame = (props) => {
   const { findOneGame } = useContext(GameContext);
   const id = props.match.params.id;
+  console.log(`this is the one game ${id}`);
   const game = findOneGame(id);
-  console.log(`this is the One Game ${game}`);
-  console.log(props);
+  console.log(game, id);
 
   return (
     <>
       <p>ONE GAME</p>
-      <h1>{game.title}</h1>
+      <h1>{game?.title}</h1>
       <div>
-        <img src={game.cover} alt={game.title} width="150px" />
+        <img src={game?.cover} alt={game?.title} width="150px" />
       </div>
-      <div>{game.description}</div>
+      <div>{game?.description}</div>
     </>
   );
 };
