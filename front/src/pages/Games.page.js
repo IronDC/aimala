399a71e5d84cbb9ff8a42de5c1a24579d82a90b7
carballo <carballet@gmail.com>
2020-04-16 16:04:10 +0200

    Card Style on Games

--- front/src/pages/Games.page.js
+++ front/src/pages/Games.page.js
@@ -7,13 +7,14 @@ import GameList from "../components/GameList";
 const AllGames = () => {
   const { games } = useContext(GameContext);
   console.log(`this is the games ${games}`);
-  const renderGameList = () => games.map((game) => <GameList game={game} key={game.id} />);
+  const renderGameList = () =>
+    games.map((game) => <GameList game={game} key={game.id} />);
 
   return (
     <>
       <ButtonBack />
       <h1>ALL GAMES</h1>
-      <ul>{renderGameList()}</ul>
+      {renderGameList()}
     </>
   );
 };
