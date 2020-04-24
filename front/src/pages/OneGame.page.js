a1ca0582c27104eacf2cbc0787eeec4e44dee33d
David Garcia <dgarciabravo13@gmail.com>
2020-04-24 17:54:32 +0200

    delete lines

--- front/src/pages/OneGame.page.js
+++ front/src/pages/OneGame.page.js
@@ -6,7 +6,6 @@ import { Link } from "react-router-dom";
 import { ButtonBack } from "../components/ButtonBack";
 import { addGametoUserFromApi } from "../../lib/apiService";
 import Button from "@material-ui/core/Button";
-
 import Container from "../components/Container";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
